da0ec1d05c0fa1f756ab598b4c71aefa7befa9f6
jobiw <jobiwankenobi37@gmail.com>
2025-05-01 16:53:48 +0200

    apk

---
+++ app/src/main/java/com/example/ocrapp/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ocrapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
